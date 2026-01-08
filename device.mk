b43b517ce9a3ea573cc7193237c10548c77ad430
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-01-08 17:40:34 +0000

    surya: Add support for Dolby Atmos (switch-OFF)
    Co-Authored-By: HELLBOY017 <abhaygill017@gmail.com>
    Co-Authored-By: CuriousNom <aryanbiswas7585@gmail.com>
    Co-Authored-By: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -456,3 +456,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
     firmware_WCNSS_qcom_cfg.ini_symlink
+
+# Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
