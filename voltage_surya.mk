47abeea155ba3e0a3942d0e6351f5c157c30c437
Ahmad Haris <ahmadharis1982@gmail.com>
2026-02-16 13:30:36 +0700

    fix for symbiotos 16

---
+++ voltage_surya.mk
@@ -0,0 +1,51 @@
+#
+# Copyright (C) 2020-2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+TARGET_SUPPORTS_OMX_SERVICE := false
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
+# Inherit from surya device
+$(call inherit-product, device/xiaomi/surya/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+
+# Lineage prebuilts
+TARGET_INCLUDES_LOS_PREBUILTS := true
+LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
+
+# Axion build flags
+TARGET_DISABLE_EPPE := true
+AXION_CAMERA_REAR_INFO := 64,13,2,2
+AXION_CAMERA_FRONT_INFO := 20
+AXION_MAINTAINER := princeofgiri
+AXION_PROCESSOR := Qualcomm®_Snapdragon™_732G
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+
+# Flashlight strength
+TORCH_STR_SUPPORTED := true
+
+# Charging
+BYPASS_CHARGE_SUPPORTED ?= true
+BYPASS_CHARGE_TOGGLE_PATH ?= /sys/class/power_supply/battery/input_suspend
+
+PRODUCT_NAME := voltage_surya
+PRODUCT_DEVICE := surya
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := M2007J20CG
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="surya_global-user 12 RKQ1.211019.001 V14.0.2.0.SJGMIXM release-keys" \
+    BuildFingerprint=POCO/surya_global/surya:12/RKQ1.211019.001/V14.0.2.0.SJGMIXM:user/release-keys
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# TARGET_KERNEL_PLATFORM_TARGET := sm6150
